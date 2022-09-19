7263208f35f5b5a90c417b0e91bfe95bb2f446b6
Lautaro Desouches <lautarodesouches@outlook.com>
2022-09-19 13:49:22 -0300

    Proyect structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class='header row align-items-center justify-content-center text-center'>
+    <div class='col-12 col-md-4'>PeticiónIpsoFacto</div>
+    <div class='col-12 col-md-4'>Ubicacion</div>
+    <div class='col-12 col-md-4'>
+      <CartIcon :cartLength='1' />
+    </div>
+  </header>
+  <main class='main'>
+    <CustomInfo msg="Welcome to Your Vue.js App" />
+    <ProductList msg="Welcome to Your Vue.js App" />
+    <UserLogin msg="Welcome to Your Vue.js App" />
+    <UserRegister msg="Welcome to Your Vue.js App" />
+  </main>
+  <footer class='footer row align-items-center justify-content-center text-center'>
+    <div class='col-12 col-md-6'>
+      
+    </div>
+    <div class='col-12 col-md-6'>
+      <span>Proyecto final Vue</span>
+    </div>
+  </footer>
 </template>
-
+<!------------------------------------------------------------------------------------------->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CustomInfo from './components/CustomInfo.vue'
+import ProductList from './components/ProductList.vue'
+import UserLogin from './components/UserLogin.vue'
+import UserRegister from './components/UserRegister.vue'
+import CartIcon from './components/CartIcon.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    CustomInfo,
+    ProductList,
+    UserLogin,
+    UserRegister,
+    CartIcon
+}
 }
 </script>
-
+<!------------------------------------------------------------------------------------------->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --primary-bg: #e7e5e4;
+  --primary-text: #000;
+  --secondary-bg: #fff;
+  --secondary-text: #000;
+}
+
+.header {
+  margin: 0;
+  width: 100vw;
+  height: 6vh;
+  background-color: var(--secondary-bg);
+  color: var(--secondary-text);
+}
+
+.main {
+  width: 100vw;
+  height: 88vh;
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+}
+
+.footer {
+  width: 100vw;
+  height: 6vh;
+  background-color: var(--secondary-bg);
+  color: var(--secondary-text);
 }
 </style>
